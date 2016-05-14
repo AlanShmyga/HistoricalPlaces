b014175c75825ed2d0c388dc2cdb90c54e85c44c
AlanShmyga <alexey.shmyga@gmail.com>
2016-05-14 17:55:37 +0300

    ChangedMavenArchetype

--- src/main/java/com/historicalplaces/core/RequestListenerServlet.java
+++ src/main/java/com/historicalplaces/core/RequestListenerServlet.java
@@ -1,5 +1,9 @@
 package com.historicalplaces.core;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /*
  * This class contains Servlet which listen requests 
  * from Authenticated and Authorized users. For this knowledge it asks 
@@ -10,6 +14,16 @@ package com.historicalplaces.core;
  * Then receives response and translate it back to user.
  */
 
-public class RequestListenerServlet {
+public class RequestListenerServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response){
+		
+	}
 
 }
