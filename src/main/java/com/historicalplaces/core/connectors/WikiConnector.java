5bd0754fc987b4bf0bfb4948d6be530414671704
nik40fox <nedubrovin@gmail.com>
2016-05-18 13:14:41 +0300

    wiki upd

--- src/main/java/com/historicalplaces/core/connectors/WikiConnector.java
+++ src/main/java/com/historicalplaces/core/connectors/WikiConnector.java
@@ -5,5 +5,15 @@ package com.historicalplaces.core.connectors;
  */
 
 public class WikiConnector {
+	
+	private String reqestBody = "https://en.wikipedia.org/w/"
+			+ "api.php?action=query&format=json&list=geosearch&gsradius=10000&gscoord=";
+	String lat;
+	String lon;
+	String request = reqestBody + lat + "%7C" + lon;
+	
+	public void sendWikiRequest(){
+		
+	}
 
 }
